57ccf66911dc8d2f2dd26ec2c1ab52682a280ba8
Yoosuf <mayoosuf@gmail.com>
2017-08-15 10:29:38 +0530

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-
 import Layout from './components/layout/Layout'
-
-
 import Home from './components/Home'
 import Explore from './components/Explore'
 import Profile from './components/Profile'
-
 
 class App extends Component {
   render() {
