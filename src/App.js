7fc7a9fe8cb46b33a9ff9299fc31d91c3321ab42
Yoosuf <mayoosuf@gmail.com>
2018-01-09 18:58:15 +0530

    route optimisation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Layout from './components/layout/Layout'
-import Home from './components/Home'
-import Explore from './components/Explore'
-import Profile from './components/Profile'
+import { Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import Home from './components/Home';
+import Explore from './components/Explore';
+import Profile from './components/Profile';
 
 export default class App extends Component {
   render() {
     return (
-      <Router>
-        <Layout>
-          <Route exact path="/" component={Home}/>
-          <Route path="/explore" component={Explore}/>
-          <Route path="/profile" component={Profile}/>
-        </Layout>
-      </Router>
+      <Layout>
+        <Route exact path="/" component={Home}/>
+        <Route path="/explore" component={Explore}/>
+        <Route path="/profile" component={Profile}/>
+      </Layout>
     );
   }
 }
